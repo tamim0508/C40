6f625a99320fcba33f440cf551c23b5d12c60ca7
Altaf Sarker <ahsarker@AHSarker-M07.local>
2020-06-29 17:49:52 -0700

    game

--- js/Game.js
+++ js/Game.js
@@ -44,7 +44,7 @@ class Game {
     form.hide();
     
     Player.getPlayerInfo();
-    
+    player.getCarsAtEnd();
     if(allPlayers !== undefined){
       background(rgb(198,135,103));
       image(track, 0,-displayHeight*4,displayWidth, displayHeight*5);
@@ -93,6 +93,13 @@ class Game {
 
     if(player.distance > 3860){
       gameState = 2;
+      player.rank +=1
+      Player.updateCarsAtEnd(player.rank);
+      textSize(30);
+      fill("blue")
+      text("Rank:" + player.rank, 500, 100);
+      
+
     }
    
     drawSprites();
@@ -100,5 +107,6 @@ class Game {
 
   end(){
     console.log("Game Ended");
+    console.log(player.rank);
   }
 }
